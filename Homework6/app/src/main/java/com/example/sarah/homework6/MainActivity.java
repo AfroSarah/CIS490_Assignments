593205a0ec45be3a6f747195da0bb382c1b58afb
AfroSarah <afrosarah@gmail.com>
2014-11-24 14:52:53 -0500

    Parse SDK and permissions for app to parse.

--- Homework6/app/src/main/java/com/example/sarah/homework6/MainActivity.java
+++ Homework6/app/src/main/java/com/example/sarah/homework6/MainActivity.java
@@ -12,6 +12,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
 
+import com.parse.Parse;
+import com.parse.ParseObject;
 
 
 public class MainActivity extends Activity {
@@ -20,12 +22,24 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Parse.initialize(this, "AFF7pE4oZajvAMU73y1ZgDTBt1d0uWx6HRBAd02i", "XznvgNa7WuG3zTW9VHYZquzI015dKsKJ2rVpXt2e");
+
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+// Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
     // broadcast a custom intent.
     public void broadcastIntent(View view)
     {
         Intent intent = new Intent();
-        intent.setAction("com.tutorialspoint.CUSTOM_INTENT");
+        intent.setAction("com.example.sarah.CUSTOM_INTENT");
         sendBroadcast(intent);
     }
 }
